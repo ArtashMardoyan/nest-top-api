b6393116603337ba8cef10f2e1c8ce9ef0667af4
artash_mardoyan <artashmardoyan@gmail.com>
2022-03-25 13:34:48 +0400

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { PostModule } from './post/post.module';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { AppService } from './app.service';
 
 @Module({
-    imports: [AuthModule, TopPageModule, ProductModule, ReviewModule, PostModule],
+    imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
     controllers: [AppController],
     providers: [AppService]
 })
